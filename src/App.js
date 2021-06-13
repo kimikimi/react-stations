c82455a557eecfe0dd82eb61a5553f015d1d56a4
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-06-13 23:42:20 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] =React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+
   return (
     <div>
       <header>Dogアプリ</header>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="" />
+      <img src={dogUrl} alt="" />
     </div>
   )
 }
