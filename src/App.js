c945297726204a978966db9f23274e701956095e
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-06-13 23:59:25 +0900

    station5

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] =React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
-
+  function ssetDogUrl(){
+    // this.useState
+    console.log("hogehoge");
+  }
   return (
     <div>
       <header>Dogアプリ</header>
       <img src={dogUrl} alt="" />
+      <button onClick={()=>setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ")}>更新</button>
     </div>
   )
 }
