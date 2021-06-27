14ccb724f421f6edbd1b2a7c158213bf43d407f7
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-06-28 00:59:55 +0900

    station9#1

--- src/App.js
+++ src/App.js
@@ -4,36 +4,25 @@ import * as React from 'react'
 import './App.css'
 import {Header} from './Header'
 import {Description} from './Description'
+import {DogListContainer} from './DogListContainer'
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] =React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+  
   return (
     <div>
       <Header title={"Dogアプリ"} />
       <Description description={"ワンちゃんの画像を集めたサイトです"}　text={"更新"} />
-      
-      
-      
+
       <br/>
       <hr/>
-      </div>
+      <DogListContainer />
+    </div>
 
-      // <button className="button"
-      // onClick={()=>fetch('https://dog.ceo/api/breeds/image/random')
-      //  .then(response =>{
-      //   console.log(response);　
-      //   return response.json();
-      //  })
-      //  .then(data => {
-      //   console.log(data);
-      //   setDogUrl(data.message);
-      // })}>{props.text}</button>
        
     
   )
 }
-// <button onClick={()=>setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ")}>更新</button>
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,24 @@
 // DO NOT DELETE
+// DO NOT DELETE
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+
+export const DogListContainer = (props) =>{
+	const [breeds, setBreeds]=useState();
+
+	useEffect(()=>{
+		// console.log("hogehoge");
+		fetch("https://dog.ceo/api/breeds/list/all")
+		.then(response=> response.json())
+		.then(result => {
+			setBreeds(Object.keys(result.message));
+			 console.log(Object.keys(result.message));
+		})
+	},[])
+	return(
+		<div>
+		hogehoge
+			// <img src={props.url} className="dog-img" alt="ワンちゃんの画像"/>
+		</div>
+		)
+};
