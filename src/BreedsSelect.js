c9c127f84cf2307877bbc08259680306e862b341
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-06-28 23:07:45 +0900

    station10#1

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,21 @@
 // DO NOT DELETE
+import * as React from 'react'
+
+export const BreedsSelect = (props) =>{
+	let breeds =[];
+	breeds = breeds.concat(props.breeds);
+	console.log("hoge",breeds);
+	console.log("foo-pops")
+	const listItems = breeds.map((breed)=>
+		<option key={breed} value={breed} text={breed}>{breed}</option>
+		)
+		console.log ({listItems});
+	return (
+	<div>
+		<select defaultValue={props.selected} onChange={props.change}>
+		<option>Plese select</option>
+		{listItems}
+		</select>
+	</div>
+	)
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -2,6 +2,7 @@
 // DO NOT DELETE
 import * as React from 'react'
 import { useState, useEffect } from 'react'
+import {BreedsSelect} from './BreedsSelect'
 
 export const DogListContainer = (props) =>{
 	const [breeds, setBreeds]=useState();
@@ -19,6 +20,7 @@ export const DogListContainer = (props) =>{
 		<div>
 		hogehoge
 			// <img src={props.url} className="dog-img" alt="ワンちゃんの画像"/>
+			<BreedsSelect breeds={breeds}/>
 		</div>
 		)
 };
