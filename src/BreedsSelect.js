d406962a47bd05770adec199e85b4e3a253d69ad
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-06-29 19:00:43 +0900

    station10#1

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -5,18 +5,18 @@ export const BreedsSelect = (props) =>{
 	let breeds =[];
 	breeds = breeds.concat(props.breeds);
 	console.log("hoge",breeds);
-	console.log("foo-pops")
+	
 	const listItems = breeds.map((breed)=>
 		<option key={breed} value={breed} text={breed}>{breed}</option>
 		)
-		// console.log ({listItems});
+		
 	return (
 	<div>
-		<select id="choice"  onChange={props.change}>
+		<select id="choice" onChange={props.change}>
 		<option>Plese select</option>
 		{listItems}
 		</select>
-		{props.selected}
+		{props.selectedBreed}
 	</div>
 	)
 }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,5 +1,5 @@
 // DO NOT DELETE
-// DO NOT DELETE
+
 import * as React from 'react'
 import { useState, useEffect } from 'react'
 import {BreedsSelect} from './BreedsSelect'
@@ -7,23 +7,24 @@ import {BreedsSelect} from './BreedsSelect'
 export const DogListContainer = (props) =>{
 	const [breeds, setBreeds]=useState();
 	const [selectedBreed,setSelectedBreed]=useState("");
+	
 	useEffect(()=>{
 		fetch("https://dog.ceo/api/breeds/list/all")
 		.then(response=> response.json())
 		.then(result => {
 			setBreeds(Object.keys(result.message));
-			 console.log(Object.keys(result.message));
+			console.log(Object.keys(result.message));
 		})
 	},[])
 	return(
 		<div>
 		hogehoge
-			// <img src={props.url} className="dog-img" alt="ワンちゃんの画像"/>
+			
 			<BreedsSelect breeds={breeds}　
 			change={()=>{
 				setSelectedBreed(document.getElementById( "choice" ).value);
 				console.log("hogehoge3",selectedBreed);} }
-			selected={selectedBreed}/>
+			selectedBreed={selectedBreed}/>
 		</div>
 		)
 };
